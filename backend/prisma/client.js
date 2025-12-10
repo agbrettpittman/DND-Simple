54f73f8a18b92d35d62c330a446578b03cdaf1e9
Brett Pittman <agbrettpittman@gmail.com>
2025-12-10 13:09:23 -0500

    Fixed backend adapter errors

--- backend/prisma/client.js
+++ backend/prisma/client.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import path from 'path'
 
 // Ensure a single PrismaClient instance across hot-reloads in dev
 const globalForPrisma = globalThis
@@ -10,9 +12,13 @@ const globalForPrisma = globalThis
 if (!process.env.DATABASE_URL) {
     process.env.DATABASE_URL = 'file:../data/db/db.sqlite'
 }
+const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL
+})
 
 let prisma = globalForPrisma.__prismaClient || new PrismaClient({
-    log: ['error', 'warn']
+    log: ['error', 'warn'],
+    adapter: adapter
 })
 
 if (process.env.NODE_ENV !== 'production') {
